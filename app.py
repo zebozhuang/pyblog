90eb9d493431394ebb247aad0d4c256b0e8056a4
zebozhuang <zebozhuang@163.com>
2015-04-29 22:38:18 +0800

    add templates

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.append('..')
+
 import tornado.web
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 
 from tornado.options import define, options
+from pyblog.settings import settings
 
 define("port", default=8000, help="run on the given port", type=int)
 
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("building blog...")
-
+        self.render("home.html")
 
 if __name__ == '__main__':
+    print settings
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r'/', IndexHandler)])
+    app = tornado.web.Application(handlers=[(r'/', IndexHandler)], **settings)
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
--- settings.py
+++ settings.py
@@ -1,8 +1,18 @@
 # utf-8
 
+import os
+
 MYSQL_DATABASE = {
         'host': '127.0.0.1',
         'user': 'root',
         'passwd': '123456',
         'db': 'pyblog',
+}
+
+settings = {
+        "static_path": os.path.join(os.path.dirname(__file__), 'static'), 
+        "template_path": os.path.join(os.path.dirname(__file__), 'templates'),
+        "cookie_secret": "adefMnxO",
+#        "login_url": "/login",
+        "xsrf_cookies": True,
 }
